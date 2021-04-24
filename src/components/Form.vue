3a474a81cc81d6593afa8b202ee4ef8e0cefeb9e
leboeuf <ml@maximeleboeuf.com>
2021-04-24 09:07:43 -0400

    Add initial support for Section

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -14,65 +14,66 @@ export default defineComponent({
     components: {
         TextBox: defineAsyncComponent(() => import('./TextBox.vue')),
         Heading: defineAsyncComponent(() => import('./Heading.vue')),
+        Section: defineAsyncComponent(() => import('./Section.vue')),
     },
     setup() {
         const components = reactive([])
         const schema = {
-            product: {
-                type: 'entity',
-                label: 'product',
-                schema: {
-                    id: {
-                        label: 'id',
-                        type: 'text',
-                        readonly: true,
-                        value: '169c7c42-e93d-4ca4-9be5-63fea0532681'
-                    },
-                    name:
-                    {
-                        label: 'name',
-                        type: 'text',
-                        placeholder: 'Name'
-                    }
-                }
-            }
-            // auto_mode: {
-            //     type: 'section',
-            //     label: 'Automatic mode',
-            //     description: 'This is the desc',
+            // product: {
+            //     type: 'entity',
+            //     label: 'product',
             //     schema: {
-            //         events_header: {
-            //             label: 'Events',
-            //             type: 'header',
+            //         id: {
+            //             label: 'id',
+            //             type: 'text',
+            //             readonly: true,
+            //             value: '169c7c42-e93d-4ca4-9be5-63fea0532681'
             //         },
-            //         send_messages: {
-            //             label: 'Send messages',
-            //             type: 'checkbox',
-            //             description: 'This text goes under the label'
-            //         },
-            //         send_read_receipts: {
-            //             label: 'Send read receipts',
-            //             type: 'checkbox',
-            //             description: 'This text goes under the label'
-            //         },
-            //         delay_header: {
-            //             label: 'Delay',
-            //             type: 'header',
-            //         },
-            //         delay_text: {
-            //             label: 'The time to wait between events.',
-            //             type: 'paragraph',
-            //         },
-            //         delay: {
-            //             label: 'seconds',
-            //             type: 'number'
+            //         name:
+            //         {
+            //             label: 'name',
+            //             type: 'text',
+            //             placeholder: 'Name'
             //         }
             //     }
             // }
+            auto_mode: {
+                type: 'section',
+                label: 'Automatic mode',
+                description: 'This is the desc',
+                schema: {
+                    events_header: {
+                        label: 'Events',
+                        type: 'heading',
+                    },
+                    send_messages: {
+                        label: 'Send messages',
+                        type: 'checkbox',
+                        description: 'This text goes under the label'
+                    },
+                    send_read_receipts: {
+                        label: 'Send read receipts',
+                        type: 'checkbox',
+                        description: 'This text goes under the label'
+                    },
+                    delay_header: {
+                        label: 'Delay',
+                        type: 'heading',
+                    },
+                    delay_text: {
+                        label: 'The time to wait between events.',
+                        type: 'paragraph',
+                    },
+                    delay: {
+                        label: 'seconds',
+                        type: 'number'
+                    }
+                }
+            }
         }
 
         const generateControls = (parent) => {
-            var node = parent ?? schema;
+            var node = parent ?? schema
             Object.keys(node).forEach((key, index) => {
                 var properties = node[key]
                 var type = properties['type']
@@ -80,22 +81,32 @@ export default defineComponent({
                     throw `Expected property 'type' on node '${key}'.`
                 }
 
-                if (type === 'entity') {
+                if (type === 'section') {
                     var label = properties['label']
                     if (label === undefined) {
-                        throw `Expected property 'label' on node '${key}' because type is 'entity'.`
+                        throw `Expected property 'label' on node '${key}' because type is 'section'.`
                     }
 
-                    components.push(generateControl({ type: 'Heading', model: { title: key } }))
+                    var description = properties['description']
+                    components.push(generateControl({ type: 'Section', model: { title: label, description: description, components: generateControls(properties['schema']) } }))
+                }
+                else if (type === 'entity' || type === 'heading') {
+                    var label = properties['label']
+                    if (label === undefined) {
+                        throw `Expected property 'label' on node '${key}' because type is '${type}'.`
+                    }
+
+                    components.push(generateControl({ type: 'Heading', model: { title: label } }))
+
+                    // TODO: should be nested component-wise, like Section, but not for Heading
+                    var nestedSchema = properties['schema']
+                    if (nestedSchema !== undefined) {
+                        generateControls(nestedSchema)
+                    }
                 } else if (type === 'text') {
                     let model = properties
                     model.name = key
                     components.push(generateControl({ type: 'TextBox', model }))
-                }
-
-                var nestedSchema = properties['schema']
-                if (nestedSchema !== undefined) {
-                    generateControls(nestedSchema)
                 }
             })
 
---
+++ src/components/Section.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script setup>
+import { defineAsyncComponent, defineComponent, reactive, onMounted } from 'vue'
+
+defineProps({
+  title: String,
+  description: String
+})
+
+const components = reactive([])
+</script>
+
+<style scoped>
+</style>
